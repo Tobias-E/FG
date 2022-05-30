e2d81e095b49500e4fbcb523e7f2406a262e6ddb
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2022-05-30 13:16:39 +0200

    Landing section added mobile

---
+++ src/components/organisms/Landing.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+// Assets, interfaces & states
+import { theme } from '../utils';
+import BG from '../../assets/img/LandingBackground.jpg';
+import { ReactComponent as CtaIcon } from '../../assets/icons/cta.svg';
+
+export const Landing: React.FC = () => {
+	return (
+		<Container>
+			<Blur>
+				<H1>
+					En verden af <br /> enkle opskrifter
+				</H1>
+				<P>Find opskrifter til bagværk, dagligsmad & de helt store middage.</P>
+			</Blur>
+			<CTA>
+				Find inspiration <CtaIcon />
+			</CTA>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	background-image: url(${BG});
+	background-repeat: no-repeat;
+	background-size: cover;
+	position: relative;
+	z-index: -1;
+`;
+
+const Blur = styled.div`
+	margin: 20vh 4rem 20vh 4rem;
+	border-radius: 10px;
+	&:after {
+		content: '';
+		backdrop-filter: blur(2px);
+		border-radius: 20px;
+		position: absolute;
+		z-index: -2;
+		top: 20vh;
+		left: 3.5rem;
+		height: 15rem;
+		width: 75vw;
+	}
+`;
+
+const H1 = styled.h1`
+	font-weight: 500;
+	color: ${theme.textColor};
+	text-align: left;
+`;
+const P = styled.p`
+	font-weight: 200;
+	color: ${theme.textColor};
+	text-align: left;
+	line-height: 1.6;
+`;
+
+const CTA = styled.button`
+	width: 70vw;
+	align-self: center;
+	padding: 1rem 2.5rem;
+	margin: 0 0 6rem 0;
+	border-radius: 50px;
+	border: none;
+	background-color: ${theme.buttonColor};
+	color: ${theme.textColor};
+	font-size: 20px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+`;
